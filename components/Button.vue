3e20fce78dca9fe74ed39262dd7cb7044df1d983
lotte <14766250+lottverw@users.noreply.github.com>
2024-11-19 10:05:18 +0100

    Added button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,6 +1,24 @@
 <template>
-    <button
-        class="px-6 py-2 bg-primary rounded-full outline-none text-white focus:ring-2 focus:ring-primary-50 hover:bg-orange">
+    <component
+        :is="component"
+        :to="to"
+        class="px-6 py-2 rounded-full outline-none focus:ring-2 mr-2"
+        :class="{ 
+            'text-white bg-primary focus:ring-primary-50 hover:bg-orange': !variant || variant === 'primary',
+            'bg-primary-50 text-primary focus:ring-secondary-50 hover:bg-primary': variant === 'secondary'
+        }">
         <slot></slot>
-    </button>
+    </component>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+     variant?: 'primary' | 'secondary',
+     to?: string, 
+    }>();
+
+const component = computed(() => {
+    if(props.to) return resolveComponent('NuxtLink');
+    return "button";
+})
+</script>
