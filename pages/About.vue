0e8caa95e7ff3b31ad55a2a717d52e3e6ae05cbe
lotte <14766250+lottverw@users.noreply.github.com>
2024-11-19 10:06:57 +0100

    Rename the about page

--- pages/About.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Over het salon</h1>
-</template>
---
+++ pages/het-salon.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Over het salon</h1>
+</template>
+<script setup lang="ts">
+definePageMeta({
+    "title": "Castor Grooming | Het salon"
+})
+</script>
